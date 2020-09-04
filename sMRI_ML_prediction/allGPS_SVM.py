753c3282e812c4aca153ad1396e5144ac956e3f5
symoon <mrn538@snu.ac.kr>
2020-09-04 15:16:37 +0900

    Downloaded Summer Codes from NIPA

---
+++ sMRI_ML_prediction/allGPS_SVM.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.model_selection import KFold, GridSearchCV, train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler, Imputer
+
+k_fold = 5
+
+all_data = pd.read_csv('/home/ubuntu/SEOYOON/BRAIN_structural/Data/abcd_smrip101_merged_with_GPS.csv', header=0)
+
+scaler = StandardScaler()
+imp = Imputer(missing_values = np.nan, strategy = 'mean')
+X_raw = all_data.iloc[:, 37:-2].values
+X = imp.fit_transform(X_raw)
+X = scaler.fit_transform(X)
+
+result = []
+gps = all_data.iloc[:, 1:27].columns
+for target in gps:
+    y = all_data[target]
+    y.values.ravel()
+    print("\n")
+    print("[{}]".format(target))
+    outer_cv = KFold(n_splits = k_fold, shuffle=False, random_state = 123)
+    loop=1
+    for train_index, test_index in outer_cv.split(X, y):
+    #     print('='*50)
+        print('Loop: ', loop)
+
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        svr = SVR(C=0.1, gamma='auto')
+        svr.fit(X_train, y_train)
+        
+        train_sc = svr.score(X_train, y_train)
+        test_sc = svr.score(X_test, y_test)
+        
+        result.append([target, train_sc, test_sc])
+        print("{:.<45}".format("1. Train r2 score"))
+        print("\t{}".format(train_sc))
+        print("{:.<45}".format("2. Test r2 score"))
+        print("\t{}".format(test_sc))
+        loop +=1
+        
+pd.DataFrame(result, columns=['GPS', 'train_score', 'test_score']).to_csv('SVM_prediction.csv', index=False)
